f0e9cbbccbac18ffef250b5bc968c44258ea7c07
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-04-04 19:56:51 -0700

    hide main() contents and Added JS code from glitch.com

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,11 +23,62 @@ class MyProjectClass {
 }
 
 function main() {
+  const fillers = {
+    adventurer: ["Pixel Hunter", "Glitch Fixer", "Code Weaver", "Digital Explorer"],
+    pre: ["Cyber", "Pixel", "Data"],
+    post: ["land", "scape", "net"],
+    people: ["pixelated", "distorted", "glitched", "digital"],
+    item: ["algorithm", "decoder ring", "USB key", "lens"],
+    num: ["countless", "a myriad of", "a handful of", "an array of"],
+    looty: ["rare", "mysterious", "lengendary"],
+    loots: ["pixels", "icons", "bitmaps"],
+    baddies: ["corruptors", "data phantoms", "pixel ghouls", "agents of chaos"],
+    message: ["transmission", "digital note", "coded message", "virtual plea","...wait, no! Come back"],
+    
+  };
+  
+  const template = `$adventurer, heed my $message!
+  
+  I have just come from $pre$post where the $people inhabitants are in conundrum. Their world has been fractured by $baddies. You must venture forth without delay, armed with my $item, and assist them in restoring harmony.
+  
+  Legend has it that the one who succeeds in this quest will be rewarded with $num $looty $loots. Such a treasure is surely enough to entice a hero of your caliber!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    box.innerText = story;
+  }
+  
+  /* global clicker */
+  clicker.onclick = generate;
+  
+  generate();
+
+
   // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  //let myInstance = new MyProjectClass("value1", "value2");
 
   // call a method on the instance
-  myInstance.myMethod();
+  //myInstance.myMethod();
 }
 
 // let's get this party started - uncomment me
