fe988261447e6c78cafb0c848cf5c84540106ffd
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-04-04 20:00:16 -0700

    Edited two lines to use jQuery

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -65,11 +65,11 @@ function main() {
     }
   
     /* global box */
-    box.innerText = story;
+    $("#box").text(story);
   }
   
   /* global clicker */
-  clicker.onclick = generate;
+  $("#clicker").click(generate);
   
   generate();
 
