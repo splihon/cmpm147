f0ead814e33a2f2dc306b5c8429de0685495949e
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-04-05 12:37:06 -0700

    Fullscreen button

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -73,6 +73,17 @@ function main() {
   
   generate();
 
+  document.getElementById("fullscreen").addEventListener("click", function(){
+    if (!document.fullscreenElement){
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.log('Error trying to enable fullscreen mode: ${err.message} (${err.name})');
+      });
+    } else {
+      if (document.exitFullscreen){
+        document.exitFullscreen();
+      }
+    }
+  });
   // create an instance of the class
   //let myInstance = new MyProjectClass("value1", "value2");
 
